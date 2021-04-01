25a4cacd9a72f786eb97b998afe6c9405352db3e
Sneakywho <81007743+Sneakywho@users.noreply.github.com>
2021-04-01 00:44:40 -0500

    Update bingo.ps1

--- bingo/bingo.ps1
+++ bingo/bingo.ps1
@@ -17,14 +17,14 @@ if ($help) {
 	exit
 }
 # Mode selection if not provided
-for ($x = "0"; $x -ne "1"; $x++){
+for ($x = 0; $x -ne 1; $x++){
 if ($mode -eq "") {
 	$mode = read-host -prompt "1) Blackout`r`n2) Rows`r`nMode (select number)" $x++
-	if($mode -ne "1" -or "2") {$x--;}
+	if($mode -ne 1 -or 2) {$x--;}
 }
-elseif ($mode -ne "1" -or "2") {
-	$mode = read-host -prompt "1) Blackout`r`n2) Rows`r`nMode (select number)" $x++
-	if($mode -ne "1" -or "2") { $x--;}
+#elseif ($mode -ne 1 -or 2) {
+#	$mode = read-host -prompt "1) Blackout`r`n2) Rows`r`nMode (select number)" $x++
+#	if($mode -ne 1 -or 2) { $x--;}
 }
 else {
 	echo "Using mode $mode."
